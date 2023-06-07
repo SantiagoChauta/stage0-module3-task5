24ce3c26b907508a22ce5517a239345d4dc13b46
Santiago Chauta <santiagochauta@gmail.com>
2023-06-07 09:49:04 -0500

    finish

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
+	
     void divide(int toBeDivided) {
+    	double ans = (double) toBeDivided/5;
+    	System.out.println(ans);
     }
 }
